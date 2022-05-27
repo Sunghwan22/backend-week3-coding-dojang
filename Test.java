69e2469c46a3d76be9386c80927a820f0e82c50a
sunghwan22 <tidls3144@naver.com>
2022-05-27 11:54:26 +0900

    3주차 코딩테스트
    2문제 다 손도 못댔네요.... 완패..

---
+++ Test.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+
+        HashMap<String,Integer> map = new HashMap<>();
+
+
+        Scanner scanner = new Scanner(System.in);
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000);
+
+
+        System.out.print("로마 숫자: " );
+        String roman = scanner.nextLine();
+
+
+
+
+
+
+
+    }
+}
---
+++ Test2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("십진수를 입력해주세요: ");
+
+        System.out.println();
+
+
+    }
+}
